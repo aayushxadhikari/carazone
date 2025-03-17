26ebe1bf306c9b85a660b10fe058d5458192d80a
aayushxadhikari <aayushxadhikari@gmail.com>
2025-03-17 12:55:09 +0545

    Engine model created

--- models/engine.go
+++ models/engine.go
@@ -1,2 +1,11 @@
 package models
 
+import "github.com/google/uuid"
+
+type Engine struct{
+	EngineID uuid.UUID `json:"engineid"`
+	Displacement int64 `json:"displacement"`
+	NoOfCylinders int64 `json:"noOfCylinders"`
+	CarRange int64 `json:"carRange"`
+
+}
