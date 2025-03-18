0bc3a52de623d9abbee42df4eea53f5368a94303
aayushxadhikari <aayushxadhikari@gmail.com>
2025-03-18 10:10:53 +0545

    Completed GetCarById function

--- store/car/car.go
+++ store/car/car.go
@@ -17,11 +17,42 @@ func new(db *sql.DB) Store{
 }
 
 func (s Store) GetCarById(ctx context.Context, id string) (models.Car, error){
+	var car models.Car
 	
+	query := `SELECT c.id, c.name, c.year, brand, c.fuel_type, c.engine_id, c.price, c.created_at,c.updated_at, e.id,
+	e.displacement, e.no_of_cylinders, e.car_range 
+	FROM car c, engine e 
+	ON c.engine_id = e.id
+	WHERE c.id=$1`
+
+	row := s.db.QueryRowContext(ctx, query, id)
+	err := row.Scan(
+		&car.ID,
+		&car.Name,
+		&car.Year,
+		&car.Brand,
+		&car.FuelType,
+		&car.Engine.EngineID,
+		&car.Price,
+		&car.CreatedAt,
+		&car.UpdateAt,
+		&car.Engine.EngineID,
+		&car.Engine.Displacement,
+		&car.Engine.NoOfCylinders,
+		&car.Engine.CarRange,
+	)
+
+	if err != nil{
+		if err== sql.ErrNoRows{
+			return car, nil
+		}
+		return car, err
+	}
+	return car, nil
 }
 
 func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool){
-
+	
 }
 
 func (s Store) CreateCar(ctx context.Context, carReq *models.CarRequest)(models.Car, error){
@@ -33,5 +64,5 @@ func (s Store) UpdateCar(ctx context.Context, id string, carReq *models.CarReque
 }
 
 func (s Store) DeleteCar(ctx context.Context, id string)(models.Car,error){
-	
+
 }
