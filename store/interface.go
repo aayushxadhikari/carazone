22beae937a4f9c11fc1787ece8ee970880b0bced
aayushxadhikari <aayushxadhikari@gmail.com>
2025-03-20 11:57:48 +0545

    services created for the car and the interface

--- store/interface.go
+++ store/interface.go
@@ -1,1 +1,16 @@
 package store
+
+import (
+	"context"
+
+	"github.com/aayushxadhikari/carazone/models"
+)
+
+type CarStoreInterface interface {
+	GetCarById(ctx context.Context, id string) (models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+	CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error)
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (models.Car,error)
+	DeleteCar(ctx context.Context, id string) (models.Car, error)
+}
+
