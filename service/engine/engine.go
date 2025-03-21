14742f287b02e473a78f1d215c257481a86700c5
aayushxadhikari <aayushxadhikari@gmail.com>
2025-03-21 10:03:27 +0545

    compeleted the Engine service

--- service/engine/engine.go
+++ service/engine/engine.go
@@ -1,3 +1,73 @@
 package engine
 
+import (
+	"context"
+	"errors"
 
+	"github.com/aayushxadhikari/carazone/models"
+	"github.com/aayushxadhikari/carazone/store"
+)
+
+type EngineService struct{
+	store store.EngineStoreInterface
+}
+
+func NewEngineService(store store.EngineStoreInterface) *EngineService{
+	return &EngineService{
+		store: store,
+	}
+}
+
+func (s *EngineService) GetEngineByID(ctx context.Context, id string) (*models.Engine, error){
+	engine, err := s.store.EngineById(ctx, id)
+	if err != nil{
+		return nil, err
+	}
+	return &engine, nil
+}
+
+func (s *EngineService) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (*models.Engine, error) {
+	engine := models.Engine{
+		Displacement:  engineReq.Displacement,
+		NoOfCylinders: engineReq.NoOfCylinders,
+		CarRange:      engineReq.CarRange,
+	}
+
+	if err := models.ValidateEngineRequest(engine); err != nil {
+		return nil, err
+	}
+
+	if s.store == nil {
+		return nil, errors.New("store is not initialized")
+	}
+	createdEngine, err := s.store.EngineCreated(ctx, engineReq) 
+	if err != nil {
+		return nil, err
+	}
+
+	return &createdEngine, nil 
+}
+
+func (s *EngineService) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest)(*models.Engine, error){
+	engine := models.Engine{
+		Displacement:  engineReq.Displacement,
+		NoOfCylinders: engineReq.NoOfCylinders,
+		CarRange:      engineReq.CarRange,
+	}
+	if err:= models.ValidateEngineRequest(engine); err != nil{
+		return nil, err
+	}
+	updatedEngine, err := s.store.EngineUpdate(ctx, id, engineReq)
+	if err != nil{
+		return nil, err 
+	}
+	return &updatedEngine, nil
+}
+
+func (s * EngineService) DeleteEngine(ctx context.Context, id string) (*models.Engine, error){
+	deletedEngine, err := s.store.EngineDelete(ctx, id)
+	if err != nil{
+		return nil, err
+	}
+	return &deletedEngine, nil
+}
