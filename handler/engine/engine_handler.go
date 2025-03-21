03e52b4672715cf35f68c6753099c6655726135d
aayushxadhikari <aayushxadhikari@gmail.com>
2025-03-21 20:54:41 +0545

    updated the engine handler

--- handler/engine/engine_handler.go
+++ handler/engine/engine_handler.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aayushxadhikari/carazone/models"
 	"github.com/aayushxadhikari/carazone/service"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -65,7 +66,7 @@ func (h *EngineHandler) CreateEngine(w http.ResponseWriter, r *http.Request){
 	}
 
 	var engineReq models.EngineRequest
-	err = json.Unmarshal(body, engineReq)
+	err = json.Unmarshal(body, &engineReq)
 	if err != nil{
 		log.Println("Error while Unmarshalling request.")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -80,7 +81,7 @@ func (h *EngineHandler) CreateEngine(w http.ResponseWriter, r *http.Request){
 	}
 
 	responseBody, err := json.Marshal(createdEngine)
-	w.WriteHeader(http.StatusInternalServerError)
+	w.WriteHeader(http.StatusCreated)
 
 	// write the response body
 	_, _ = w.Write(responseBody)
@@ -128,7 +129,7 @@ func (h *EngineHandler) UpdateEngine(w http.ResponseWriter, r *http.Request){
 	_,_= w.Write(resBody)
 }
 
-func (h *EngineHandler) DeleteCar(w http.ResponseWriter, r *http.Request){
+func (h *EngineHandler) DeleteEngine(w http.ResponseWriter, r *http.Request){
 	ctx := r.Context()
 	params := mux.Vars(r)
 	id := params["id"]
@@ -137,22 +138,31 @@ func (h *EngineHandler) DeleteCar(w http.ResponseWriter, r *http.Request){
 	if err!= nil{
 		log.Println("Error while deleting the car:", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		response := map[string]string {"error":"Invalid ID or Engine not found"}
+		jsonResponse, _ := json.Marshal(response)
+		_,_ = w.Write(jsonResponse)
 		return 
 	}
-	body, err := json.Marshal(deletedEngine)
+	if deletedEngine.EngineID == uuid.Nil{
+		w.WriteHeader(http.StatusNotFound)
+		response := map[string]string{"error":"Engine Not Found"}
+		jsonResponse, _ := json.Marshal(response)
+		_,_= w.Write(jsonResponse)
+		return
+	}
+
+	jsonResponse, err := json.Marshal(deletedEngine)
 	if err != nil{
-		log.Println("Error while Marshalling the response:", err)
+		log.Println("Error while marshalling deleted engine response:",err)
 		w.WriteHeader(http.StatusInternalServerError)
+		response := map[string]string{"error":"Internal server error"}
+		jsonResponse, _ := json.Marshal(response)
+		_,_ = w.Write(jsonResponse)
 		return
 	}
 
 	w.Header().Set("Content-Type","application/json")
 	w.WriteHeader(http.StatusOK)
-
-	// write the response body
-	if _, err := w.Write(body); err != nil{
-		log.Println("Error Writing Response:", err)
-	}
-
-
+	_,_ = w.Write(jsonResponse)
 }
+
