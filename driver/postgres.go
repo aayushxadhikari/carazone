4dd445bee0a00b8c4087363cd098b8110a0565e2
aayushxadhikari <aayushxadhikari@gmail.com>
2025-03-23 10:31:26 +0545

    updated postgres connection

--- driver/postgres.go
+++ driver/postgres.go
@@ -6,41 +6,47 @@ import (
 	"log"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq" // Import PostgreSQL driver
 )
 
 var db *sql.DB
 
-func InitDB(){
+func InitDB() {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-	os.Getenv("DB_HOST"),
-	os.Getenv("DB_PORT"),
-	os.Getenv("DB_USER"),
-	os.Getenv("DB_PASSWORD"),
-	os.Getenv("DB_NAME"),)
+	fmt.Println("Waiting for the database to start up...")
+	time.Sleep(5 * time.Second)
 
-	fmt.Println("Waiting for the database Start Up....")
-	time.Sleep(5*time.Second)
-
-	db, err := sql.Open("postgres", connStr)
-	if err!= nil{
-		log.Fatalf("Error Opening database: %v", err)
+	var err error 
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
 	}
 
 	err = db.Ping()
-	if err != nil{
-		log.Fatalf("Error Connecting to the database : %v", err)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
 	}
 
-	fmt.Println("Successfully Connected to the database")
+	fmt.Println("Successfully connected to the database")
 }
 
-func GetDB() *sql.DB{
+func GetDB() *sql.DB {
 	return db
 }
 
-func CloseDB(){
-	if err := db.Close(); err != nil{
-		log.Fatal("Error Closing the Database: %v", err)
+func CloseDB() {
+	if db != nil {
+		if err := db.Close(); err != nil {
+			log.Fatalf("Error closing the database: %v", err)
+		}
 	}
 }
